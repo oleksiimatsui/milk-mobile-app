1c9586460092b1dd680694a3368e2b3d4d824913
SergiyLapiuk <sergiy.lapiuk@knu.ua>
2023-11-24 22:37:29 +0200

    feat: addMap

--- app/src/main/java/com/example/milkmobileapp/ui/contacts/ContactsFragment.java
+++ app/src/main/java/com/example/milkmobileapp/ui/contacts/ContactsFragment.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.milkmobileapp.Contact;
+import com.example.milkmobileapp.R;
 import com.example.milkmobileapp.databinding.FragmentContactsBinding;
+import com.example.milkmobileapp.ui.editMilk.EditMilkFragment;
+import com.example.milkmobileapp.ui.map.MapsFragment;
+import com.example.milkmobileapp.ui.milk.MilkFragment;
 
 
 import java.util.ArrayList;
@@ -44,6 +51,16 @@ public class ContactsFragment extends Fragment {
             }
             ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arr);
             listView.setAdapter(itemsAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    // Тут ви виконуєте перехід до MapsFragment
+                    MapsFragment mapsFragment = new MapsFragment();
+                    NavHostFragment.findNavController(ContactsFragment.this)
+                            .navigate(R.id.action_nav_contacts_to_mapsFragment, mapsFragment.getArguments());
+                }
+            });
         }
     };
     @Override
